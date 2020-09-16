d8fcaca09bf467f7fde0ab4f6fdb3eb2467c5fc6
crisjf <crisjf@mit.edu>
2020-09-16 16:25:13 -0400

    reset doc again

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ cd ..
 git clone https://github.com/CityScope/CS_Brix.git
 mkdir action-target/cs_brix
 mkdir action-target/cs_brix/docs
-cp -r CS_Brix/docs/markdown action-target/cs_brix/docs
+cp -r CS_Brix/docs/markdown/ action-target/cs_brix/docs
 cd action-target
