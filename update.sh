12e5ff06dc546ef701383797b358b01febd6e4b3
crisjf <crisjf@mit.edu>
2020-09-16 16:24:00 -0400

    create dire in the right place

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 echo "hello world" && ls -l && pwd
 cd ..
 git clone https://github.com/CityScope/CS_Brix.git
-mkdir cs_brix
-mkdir cs_brix/docs
+mkdir action-target/cs_brix
+mkdir action-target/cs_brix/docs
 cp -r CS_Brix/docs/markdown action-target/cs_brix/docs
 cd action-target
