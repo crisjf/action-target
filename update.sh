ca75e618575e6c7f7ed16aa267ac51cba4fe16d0
crisjf <crisjf@mit.edu>
2020-09-16 16:03:19 -0400

    git status

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 echo "hello world" && ls -l && pwd
 cd ..
 git clone https://github.com/CityScope/CS_Brix.git
-ls -l
 cp -r CS_Brix/docs/markdown action-target/cs_brix/docs/markdown
-ls -l 
-cd action-target/cs_brix/docs/markdown
-ls -l
+cd action-target
+git status
+git add .
+git status
+git 
