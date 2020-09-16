82f328c4be8517863f734dc9a51c01d58fa29207
crisjf <crisjf@mit.edu>
2020-09-16 16:00:30 -0400

    update cipy

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ echo "hello world" && ls -l && pwd
 cd ..
 git clone https://github.com/CityScope/CS_Brix.git
 ls -l
-cp CS_Brix/docs/markdown action-target/cs_brix/docs
+cp -r CS_Brix/docs/markdown action-target/cs_brix/docs/markdown
 ls -l 
-cd action-target/cs_brix/docs
+cd action-target/cs_brix/docs/markdown
 ls -l
