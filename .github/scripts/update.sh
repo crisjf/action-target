e6ec90d3d0d85630808b9f2ed2a5f85296205af5
crisjf <crisjf@mit.edu>
2020-09-17 19:00:44 -0400

    destination update

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -4,5 +4,5 @@ git clone https://github.com/${1}/${2}.git
 echo "Copying docs from: ${2}/${3:-README.md}"
 echo "Copying docs into: action-target/docsite/docs/${4:-/}$"
 mkdir -p action-target/docsite/${2}/${4:-/}
-cp -r ${2}/${3:-README.md} action-target/docsite/docs/${4:-/}${2}/
+cp -r ${2}/${3:-README.md} action-target/docsite/docs/${4:-}${2}/
 cd action-target
