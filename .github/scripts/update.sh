67edf0e93b2b7fee00a756a15754c0d872c98a21
crisjf <crisjf@mit.edu>
2020-09-17 19:02:23 -0400

    full path in destination

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -3,6 +3,6 @@ echo "Cloning repo: https://github.com/${1}/${2}.git"
 git clone https://github.com/${1}/${2}.git
 echo "Copying docs from: ${2}/${3:-README.md}"
 echo "Copying docs into: action-target/docsite/docs/${4:-/}$"
-mkdir -p action-target/docsite/${2}/${4:-/}
-cp -r ${2}/${3:-README.md} action-target/docsite/docs/${4:-}${2}/
+mkdir -p action-target/${4:-docsite/docs}/${2}/
+cp -r ${2}/${3:-README.md} action-target/${4:-docsite/docs}/${2}/
 cd action-target
