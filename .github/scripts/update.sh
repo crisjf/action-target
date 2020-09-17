4e5693bc68d7c55802e26d41ce921ee7b27b81be
crisjf <crisjf@mit.edu>
2020-09-17 18:58:03 -0400

    pull docs update

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,8 +1,8 @@
 cd ..
 echo "Cloning repo: https://github.com/${1}/${2}.git"
 git clone https://github.com/${1}/${2}.git
-echo "Copying docs from: ${2}/${3:-docs/markdown}/*"
-echo "Copying docs into: action-target/${2}/${4:-docs}/"
-mkdir -p action-target/${2}/${4:-docs}/
-cp -r ${2}/${3:-docs/markdown}/* action-target/${2}/${4:-docs}/
+echo "Copying docs from: ${2}/${3:-README.md}"
+echo "Copying docs into: action-target/docsite/docs/${4:-/}$"
+mkdir -p action-target/docsite/${2}/${4:-/}
+cp -r ${2}/${3:-README.md} action-target/docsite/docs/${4:-/}${2}/
 cd action-target
