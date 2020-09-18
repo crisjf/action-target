f447da0b752bca2fe3d8d8c5b646185cfab4b43a
crisjf <crisjf@mit.edu>
2020-09-18 10:19:39 -0400

    changed to curl

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -1,8 +1,12 @@
 cd ..
-echo "Cloning repo: https://github.com/${1}/${2}.git"
-git clone https://github.com/${1}/${2}.git
-echo "Copying docs from: ${2}/${3:-README.md}"
-echo "Copying docs into: action-target/docsite/docs/${4:-/}$"
-mkdir -p action-target/${4:-docsite/docs}/${2}/
-cp -r ${2}/${3:-README.md} action-target/${4:-docsite/docs}/${2}/
-cd action-target
+curl -o ${2}.md https://github.com/${1}/${2}/blob/master/${4:-README.md}
+ls -l 
+# echo "Cloning repo: https://github.com/${1}/${2}.git"
+# git clone https://github.com/${1}/${2}.git
+# echo "Copying docs from: ${2}/${4:-README.md}"
+# echo "Copying docs into: action-target/docsite/docs/${3:-/}$"
+# mkdir -p action-target/${3:-docsite/docs}/${2}/
+# touch action-target/${3:-docsite/docs}/${2}/${2}.md
+# printf "---\nid: ${2}\n---\n\n" > action-target/${3:-docsite/docs}/${2}/${2}.md
+# cat action-target/${3:-docsite/docs}/${2}/${2}.md ${2}/${4:-README.md} > action-target/${3:-docsite/docs}/${2}/${2}.md
+# cd action-target
